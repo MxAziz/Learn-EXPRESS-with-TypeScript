f9cc5158f67cb0c89d86a3d22c1155b43d3305ea
MxAziz <hereabdulaziz@gmail.com>
2025-06-16 01:53:51 +0600

    added a get request for all todos and import todo.json file from outside

--- dist/app.js
+++ dist/app.js
@@ -4,8 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const filePath = path_1.default.join(__dirname, "../db/todo.json");
 app.get('/', (req, res) => {
-    res.send('Hello World!!!!');
+    res.send('welcome to the Todo API');
+});
+app.get('/todos', (req, res) => {
+    const data = fs_1.default.readFileSync(filePath, 'utf-8');
+    console.log(data);
+    res.json(data);
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import express, { Application, Request, Response } from 'express'
+import path from 'path'
+import fs from 'fs'
 const app : Application = express()
 
+app.use(express.json())
+
+const filePath = path.join(__dirname, "../db/todo.json")
+
 app.get('/', (req : Request, res: Response) => {
-  res.send('Hello World!!!!')
+  res.send('welcome to the Todo API')
+
+})
+
+app.get('/todos', (req: Request, res: Response) => {
+  const data = fs.readFileSync(filePath, 'utf-8')
+  console.log(data);
+  res.json(data);
 })
 
 export default app;
