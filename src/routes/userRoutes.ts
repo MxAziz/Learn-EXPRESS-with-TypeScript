7eb2fd2e44e60c602e54c802f5c7b64715cff743
MxAziz <hereabdulaziz@gmail.com>
2025-06-19 22:54:37 +0600

    error handling in routing system

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -9,7 +9,12 @@ router.post('/add', (req, res) => {
   res.send('নতুন ইউজার তৈরি হয়েছে');
 });
 
-
+router.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found!',
+  });
+});
 
 
 export default router;
